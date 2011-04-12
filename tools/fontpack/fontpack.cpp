4996669f6317dffe782ac84b951aa72a5cfdb54b
Joel Davis <joeld42@gmail.com>
2011-04-12 01:32:34 +0000

    [luddite] just starting to mess with this
    git-svn-id: http://luddite.googlecode.com/svn/trunk@11 3faf8e55-23c5-385e-49f0-f3670cc2fc50

--- tools/fontpack/fontpack.cpp
+++ tools/fontpack/fontpack.cpp
@@ -7,7 +7,40 @@ int main( int argc, char *argv[] )
 {
     FT_Library ftLibrary;
     FT_Init_FreeType( &ftLibrary );
-    
-    
-	printf("Fontpack whee!!\n");
+
+    int ndx = 1;
+    while (ndx < argc)
+    {
+        std::string arg = argv[ndx++];
+        if (arg=="-font")
+        {
+            int pxlSize = 12;
+            std::string fontArg = argv[ndx++];
+            
+            std::string fontfile;
+
+            // Did specify font size, too?
+            size_t separator_pos = fontArg.rfind( ':' );
+            if (separator_pos != std::string::npos)
+            {
+                fontfile = fontArg.substr( 0, separator_pos );
+                std::string sz = fontArg.substr( separator_pos+1 );
+                printf("sz is %s\n", sz.c_str() );
+                
+                pxlSize = atoi( sz.c_str() );
+            }
+            else
+            {
+                fontfile = fontArg;                
+            }
+            
+
+            printf( "TODO: set font %s size %d\n",
+                    fontfile.c_str(),
+                    pxlSize );            
+            
+        }
+        
+    }    
+
 }
